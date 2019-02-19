146e66d74bf4775fd5a243bb0466f4daff325d8c
Suraj Shrestha <shresthasuraj62@gmail.com>
2019-02-19 13:20:10 -0600

    Continuw with Image Conversion to Black and White

--- imageBW.py
+++ imageBW.py
@@ -4,16 +4,10 @@ import numpy as np
 #This function converts a colorful image to black and white so that
 # # pytesseract can have ease to recognize the texts.
 #  
-def imageChange(imagefile):
-    image=Image.open(imagefile)
-    image=np.float32(image)
-    print(type(image[0][0]))
-    each=list(image[0][0])
-    print(each)
-
-    # for x in range (0,len(image)):
-    #     for y in range (0,len(image[x])):
-    #         print (str(image[x][y]))
-#            if not(image[x][y]=[0.0,0.0,0.0]):
-#                image[x][y]=[255.0,255.0,255.0]
-imageChange("sample.jpg")
+image1=Image.open("sample.jpg")
+image1=np.float32(image1)
+for x in range (0,len(image1)):
+    for y in range (0,len(image1[x])):
+        if not((image1[x][y]==[0.0,0.0,0.0]).all()):
+            image1[x][y]=[255.0,255.0,255.0]
+image1.save("sampleBW.jpg")
