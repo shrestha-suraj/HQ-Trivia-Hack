3b1b706f07faa65940d90507b50c527422029f08
Bi-Mars <bimarshsharma101@gmail.com>
2019-02-19 20:42:32 -0500

    Bimarsh first commit

--- imageBW.py
+++ imageBW.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import numpy as np
+import main as suraj
 
 #This function converts a colorful image to black and white so that
 # # pytesseract can have ease to recognize the texts.
 #  
 image1=Image.open("sample.jpg")
+
 image1=np.float32(image1)
 for x in range (0,len(image1)):
     for y in range (0,len(image1[x])):
